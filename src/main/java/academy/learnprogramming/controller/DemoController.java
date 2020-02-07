a5d9cbd9bb91dc78df4a2e3ed917cbe82ee8083d
Ricky <rickyshi0823@gmail.com>
2020-02-06 17:00:20 -0700

    Model and Model Attributes

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,10 +1,14 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //Controller is a special type for component for controller.
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -16,10 +20,20 @@ public class DemoController {
     }
 
     /*http://localhost:8080/to*do-list/welcome*/
-    // prefix + name + suffix
-    // /WEB-INF/view/welcome.jsp
+    // 1st way to add model
     @GetMapping("welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("user", "Kai");
+        log.info("model={}", model);
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    // end way to use annotation to add model
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo App.";
     }
 }
