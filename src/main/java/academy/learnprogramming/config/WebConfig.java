7111a3d8b2772255f9d5705433bead7a9df7ea11
Ricky <rickyshi0823@gmail.com>
2020-02-10 16:58:41 -0700

    add more functions

--- src/main/java/academy/learnprogramming/config/WebConfig.java
+++ src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,17 +1,20 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "academy.learnprogramming")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
     public static final String RESOLVER_SUFFIX = ".jsp";
 
@@ -22,5 +25,11 @@ public class WebConfig {
         viewResolver.setPrefix(RESOLVER_PREFIX);
         viewResolver.setSuffix(RESOLVER_SUFFIX);
         return viewResolver;
+    }
+
+    //static url href, no need to set another controller for this
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
     }
 }
